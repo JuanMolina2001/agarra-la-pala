adadddcfd727f4404af09aba83d527bf22c51e2e
JuanMolina2001 <juanmolinajm2001@gmail.com>
2024-07-24 06:00:30 -0400

    chore: Update server port to 3000

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ import { dirname } from 'path';
 import path from 'path';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 3000;
 const app = express();
 app.use(cors({
     origin: process.env.CORS_ORIGIN || "*"
